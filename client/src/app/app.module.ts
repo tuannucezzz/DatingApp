c030f3b66505b9dd6684c9aa4276d2d28478b467
TUNA\vantu <vantuan100301@gmail.com>
2022-06-07 15:08:58 +0700

    End of Section 10

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -23,6 +23,9 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberCardsComponent } from './members/member-cards/member-cards.component';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -37,7 +40,8 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
     TestErrorsComponent,
     NotFoundComponent,
     ServerErrorComponent,
-    MemberCardsComponent
+    MemberCardsComponent,
+    MemberEditComponent
   ],
   imports: [
     BrowserModule,
@@ -46,11 +50,13 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
     CommonModule,
     BrowserAnimationsModule,
     FormsModule,
-    SharedModule
+    SharedModule,
+    NgxSpinnerModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
-    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true}
   ],
   bootstrap: [AppComponent]
 })
--- client/src/app/members/member-lists/member-lists.component.ts
+++ client/src/app/members/member-lists/member-lists.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 
@@ -8,17 +9,11 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-lists.component.css']
 })
 export class MemberListsComponent implements OnInit {
-  members: Member[];
+  members$: Observable<Member[]>;
 
   constructor(private memberService: MembersService) { }
 
   ngOnInit(): void {
-    this.loadMembers();
-  }
-
-  loadMembers() {
-    this.memberService.getMembers().subscribe(members => {
-      this.members = members;
-    })
+    this.members$ = this.memberService.getMembers();
   }
 }
