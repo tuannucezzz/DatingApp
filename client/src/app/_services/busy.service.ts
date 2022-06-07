8cf8d69b7682a9a2cc0fd7eb2e9dc7f8b29c28d5
TUNA\vantu <vantuan100301@gmail.com>
2022-06-07 22:23:24 +0700

    End of Section 11

--- client/src/app/_services/busy.service.ts
+++ client/src/app/_services/busy.service.ts
@@ -14,7 +14,7 @@ export class BusyService {
     this.spinnerService.show(undefined, {
       type: 'line-scale-party',
       bdColor: 'rgba(255, 255, 255, 0)',
-      color: '#333333'
+      color: '#ffc107'
     })
   }
 
